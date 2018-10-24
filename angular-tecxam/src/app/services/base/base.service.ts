afee012aa5c287c02e2080d2bbbb112a98d5b202
Enrique Garcia <garcia.enrique.torres@gmail.com>
2018-10-24 14:51:22 -0500

    Base service edit

--- angular-tecxam/src/app/services/base/base.service.ts
+++ angular-tecxam/src/app/services/base/base.service.ts
@@ -15,52 +15,41 @@ export class BaseService {
   }
 
   post(url: string, body: any) {
-    const token = this.tokenExtractor.getToken() as string;
-    if (token) {
-      // This is because the request needs the X-CSRFToken header, and it needs to be added manually
-      const headers = new HttpHeaders(
-        {
-          'X-CSRFToken': token,
-        }
-      );
-
-      return this._http.post<ServerResponse>(environment.apiEndpoint + url, body, { withCredentials: true, headers: headers });
-    }
-    return this._http.post<ServerResponse>(environment.apiEndpoint + url, body, { withCredentials: true });
+    return this._http.post<ServerResponse>(environment.apiEndpoint + url, body);
   }
 
-  put(url: string, body: any) {
-    const token = this.tokenExtractor.getToken() as string;
-    if (token) {
-      // This is because the request needs the X-CSRFToken header, and it needs to be added manually
-      const headers = new HttpHeaders(
-        {
-          'X-CSRFToken': token,
-        }
-      );
-
-      return this._http.put<ServerResponse>(environment.apiEndpoint + url, body, { withCredentials: true, headers: headers });
-    }
-    return this._http.put<ServerResponse>(environment.apiEndpoint + url, body, { withCredentials: true });
-  }
-
-  delete(url: string, body: any) {
-    const token = this.tokenExtractor.getToken() as string;
-    const httpOptions = {
-      withCredentials: true,
-      body: body,
-      headers: null
-    };
-    if (token) {
-      // This is because the request needs the X-CSRFToken header, and it needs to be added manually
-      const headers = new HttpHeaders(
-        {
-          'X-CSRFToken': token,
-        }
-      );
-      httpOptions.headers = headers;
-      return this._http.delete(environment.apiEndpoint + url, httpOptions);
-    }
-    return this._http.delete(environment.apiEndpoint + url, httpOptions);
-  }
+  // put(url: string, body: any) {
+  //   const token = this.tokenExtractor.getToken() as string;
+  //   if (token) {
+  //     // This is because the request needs the X-CSRFToken header, and it needs to be added manually
+  //     const headers = new HttpHeaders(
+  //       {
+  //         'X-CSRFToken': token,
+  //       }
+  //     );
+  //
+  //     return this._http.put<ServerResponse>(environment.apiEndpoint + url, body, { withCredentials: true, headers: headers });
+  //   }
+  //   return this._http.put<ServerResponse>(environment.apiEndpoint + url, body, { withCredentials: true });
+  // }
+  //
+  // delete(url: string, body: any) {
+  //   const token = this.tokenExtractor.getToken() as string;
+  //   const httpOptions = {
+  //     withCredentials: true,
+  //     body: body,
+  //     headers: null
+  //   };
+  //   if (token) {
+  //     // This is because the request needs the X-CSRFToken header, and it needs to be added manually
+  //     const headers = new HttpHeaders(
+  //       {
+  //         'X-CSRFToken': token,
+  //       }
+  //     );
+  //     httpOptions.headers = headers;
+  //     return this._http.delete(environment.apiEndpoint + url, httpOptions);
+  //   }
+  //   return this._http.delete(environment.apiEndpoint + url, httpOptions);
+  // }
 }
