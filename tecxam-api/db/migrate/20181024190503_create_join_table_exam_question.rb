4982bff336ad8018fa8506d6911ddbbb0beb239b
diegotf30 <diegotf30@gmail.com>
2018-10-24 14:05:43 -0500

    Added JoinTable migration

---
+++ tecxam-api/db/migrate/20181024190503_create_join_table_exam_question.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableExamQuestion < ActiveRecord::Migration[5.2]
+  def change
+    create_join_table :exams, :questions do |t|
+      t.index [:exam_id, :question_id]
+      t.index [:question_id, :exam_id]
+    end
+  end
+end
